00ca1dc4a417c5ba1487be8ed7eaed990ef37a22
helenphillips <helen.r.phillips@googlemail.com>
2018-10-26 15:16:04 +0200

    New file to start making figures for the manuscript

---
+++ 4_Figure.R
@@ -0,0 +1,8 @@
+
+########################################################
+# 1. Set Working Directory
+########################################################
+
+if(Sys.info()["nodename"] == "IDIVNB193"){
+  setwd("C:\\Users\\hp39wasi\\sWormDatabaseRelease")
+}
